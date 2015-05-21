892be4b889a212bf9816e68685f8f893302b737b
Alex Gaudio <adgaudio@gmail.com>
2015-05-21 01:21:52 -0400

    Preparing release 1.0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='argparse_tools',
-    version='1.0.6.dev0',
+    version='1.0.6',
     description=(
         'Share a standardized set of argparse arguments within your codebase'),
     long_description="Check the project homepage for details",
